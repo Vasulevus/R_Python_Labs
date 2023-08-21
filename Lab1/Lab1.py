a83913c9faaf202c79f4ed163ceb9042475357e4
Vasulevus <Vasulevus@gmail.com>
2023-08-21 14:53:20 +0300

    work in progress 2

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,14 +1,32 @@
 import pandas
 import numpy
 import matplotlib
+import dplython
 
 
 flats = pandas.read_csv("C:/Users/Vasulevus/Documents/Github/R_Python_Labs/Lab1/flats.csv",decimal=",")
 
 flats = pandas.DataFrame(flats)
 
-print(flats.info()) #дивимося структуру
-print(flats.head())  #показуємо перші  рядки
-print(flats.describe()) #показуємо статистичні показники датасету
-flats.columns #назви полів/імена датафрейму
+#print(flats.info()) #дивимося структуру
+#print(flats.head())  #показуємо перші  рядки
+#flats.columns #назви полів/імена датафрейму
+#print(flats.describe()) #показуємо статистичні показники датасету
 
+
+
+#flats = flats[flats.columns[0]].value_counts().sort_values(ascending=False).reset_index() #рахуємо рядки відносно поля Місто, сортуємо рядки та перезавантажуємо індекс
+
+flats = flats[flats["Місто"] != "Києво-Святошинський"] #видаляємо києво-святошинський
+
+#(flats[flats["Кімнат"] == 3 ])[["Місто"]].value_counts().sort_values(ascending=True).reset_index() #відфільтровано трьохкімнатні квартири, пораховано, відсортовано та перезавантажено індекс
+
+#(flats[flats["Кімнат"] == 2])[["Місто"]].value_counts().sort_values(ascending=False).reset_index()
+
+
+#(flats[flats["Кімнат"] == 2]).groupby(["Місто"]).aggregate({"Загальна_площа":"max"}).reset_index()#згруповано по місту
+
+a = (flats[flats["Кімнат"] == 2])
+#a.aggregate({"Загальна_площа":"min","Загальна_площа":a.std()})
+#a.groupby(["Місто"])["Загальна_площа"].std().reset_index()
+a.groupby(["Місто"]).aggregate({"Загальна_площа":["mean","std"]}).reset_index() #групуємо по місту визначаємо середнє значення та стандартне відхилення
