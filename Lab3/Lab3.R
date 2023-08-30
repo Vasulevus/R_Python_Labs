7ff551cc945fdf932cbbb0efd3334ef5e8ceece9
Vasulevus <Vasulevus@gmail.com>
2023-08-30 17:07:32 +0300

    change dataset1
    change dataset1, add .py and .R files

---
+++ Lab3/Lab3.R
@@ -0,0 +1,11 @@
+
+setwd("C:/Users/Vasulevus/Documents/Github/R_Python_Labs/Lab3")
+
+#diamonds <- read.csv('diamonds.csv', encoding = "UTF-8")
+anscombes <- read.csv('anscombe.csv', encoding = "UTF-8")
+print(
+anscombes
+)
+#head(anscombes)
+
+#write.csv(anscombe, 'ancscombe.csv') #запис даних в csv
